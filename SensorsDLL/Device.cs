810609b7479365e0dfee33de5143e600a646d3e9
Vladimir Minkin <vladimir.minkin@interactivelab.ru>
2014-05-05 19:07:31 +0400

    Initial Commit

---
+++ SensorsDLL/Device.cs
@@ -0,0 +1,229 @@
+﻿using Windows.Phone.Devices.Power;
+using Microsoft.Devices.Sensors;
+using Microsoft.Phone.Shell;
+using System;
+using UnityEngine;
+using Compass       = Microsoft.Devices.Sensors.Compass;
+using Gyroscope     = Microsoft.Devices.Sensors.Gyroscope;
+using Motion        = Microsoft.Devices.Sensors.Motion;
+using Quaternion    = Microsoft.Xna.Framework.Quaternion;
+using Vector3       = Microsoft.Xna.Framework.Vector3;
+using Microsoft.Xna.Framework;
+
+namespace Device
+{
+    public class Sensors
+    {
+
+        static private Compass          compass;
+        static private float            compassMagneticHeading;
+        static private float            compassTrueHeading;
+        static private bool             componentOutput = true;
+
+        static private Gyroscope        gyro;
+        static private DateTimeOffset   gyroLastUpdateTime      = DateTimeOffset.MinValue;
+        static private Vector3          gyroCurrentRotationRate = Vector3.Zero;
+        static private Vector3          gyroCumulativeRotation = Vector3.Zero;
+        static private Vector3          gyroCumulativeRotationOutput = Vector3.Zero;
+
+        static private Motion           motion;
+        static private AttitudeReading  motionAttitudeReading;
+        static private Vector3          motionGravity;
+
+        static public string GetDeviceName
+        {
+            get
+            {
+                return Microsoft.Phone.Info.DeviceStatus.DeviceName;
+            }
+        }
+
+        static public void SetAlwaysIdleMode()
+        {
+            PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Disabled;
+            PhoneApplicationService.Current.ApplicationIdleDetectionMode = IdleDetectionMode.Disabled;
+        }
+
+        static public float BatteryPercent
+        {
+            get { return Battery.GetDefault().RemainingChargePercent; }
+        }
+
+        static public bool CompassIsAvailable
+        {
+            get { return Compass.IsSupported; }
+        }
+
+        static public float CompassMagneticHeading
+        {
+            get { return compassMagneticHeading; }
+        }
+
+        static public float CompassTrueHeading
+        {
+            get  { return compassTrueHeading; }
+        }
+
+        static public bool CompassStart()
+        {
+            bool result = CompassIsAvailable;
+            if (result)
+            {
+                if (compass == null) { 
+                    compass = new Compass();
+                    compass.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
+                }
+                compass.CurrentValueChanged += handler_Compass;
+                compass.Start();
+            }
+            return result;
+        }
+
+        static public void CompassStop()
+        {
+            compass.Stop();
+            compass.CurrentValueChanged -= handler_Compass;
+            compass.Dispose();
+        }
+
+        
+        static public bool GyroscopeIsAvailable
+        {
+            get { return Gyroscope.IsSupported; }
+        }
+
+        static public bool GyroscopeStart(bool componentsOutput)
+        {
+            bool result = GyroscopeIsAvailable;
+            componentOutput = componentsOutput;
+            if (result)
+            {
+                gyroCumulativeRotation = Vector3.Zero;
+                gyroCumulativeRotationOutput = Vector3.Zero;
+
+                gyro = new Gyroscope();
+                gyro.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
+                gyro.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<GyroscopeReading>>(handler_Gyroscope);
+                try
+                {
+                    gyro.Start();
+                }
+                catch (InvalidOperationException)
+                {
+                    result = false;
+                }  
+            }
+            return result;
+        }
+
+        static public void GyroscopeStop()
+        {
+            gyro.Stop();
+            gyro.CurrentValueChanged -= handler_Gyroscope;
+            gyro.Dispose();
+        }
+
+        static public UnityEngine.Vector3 GyroscopeCurrentRotation
+        {
+            get { return gyroCumulativeRotation.ToUnityVector3() * Mathf.Rad2Deg; }
+        }
+
+        static public float GyroscopeCurrentRotationX
+        {
+            get { return gyroCumulativeRotation.X; }
+        }
+
+        static public float GyroscopeCurrentRotationY
+        {
+            get { return gyroCumulativeRotation.Y; }
+        }
+
+        static public float GyroscopeCurrentRotationZ
+        {
+            get { return gyroCumulativeRotation.Z; }
+        }
+
+        static public bool MotionIsAvailable
+        {
+            get
+            {
+                return Motion.IsSupported;
+            }
+        }
+        static public bool MotionStart()
+        {
+            bool result = MotionIsAvailable;
+            if (result)
+            {
+                motion = new Motion();
+                motion.TimeBetweenUpdates = TimeSpan.FromMilliseconds(20);
+                motion.CurrentValueChanged += handler_Motion;
+                motion.Start();
+            }
+            return result;
+        }
+
+        static public void MotionStop()
+        {
+            motion.Stop();
+            motion.CurrentValueChanged -= handler_Motion;
+            motion.Dispose();
+        }
+
+        static public UnityEngine.Vector3 MotionGravity
+        {
+            get { return motionGravity.ToUnityVector3() * Mathf.Rad2Deg; }
+        }
+
+        static public UnityEngine.Quaternion MotionAttitude
+        {
+            get { return motionAttitudeReading.Quaternion.ToUnityQuaternion(); }
+        }
+
+        static private void handler_Compass(object sender, SensorReadingEventArgs<CompassReading> e)
+        {
+            compassTrueHeading = (float)e.SensorReading.TrueHeading;
+            compassMagneticHeading = (float)e.SensorReading.MagneticHeading;
+        }
+
+        static private void handler_Gyroscope(object sender, SensorReadingEventArgs<GyroscopeReading> e) 
+        {
+            if (gyroLastUpdateTime.Equals(DateTimeOffset.MinValue)){
+                gyroLastUpdateTime = e.SensorReading.Timestamp;
+            }
+            else
+            {
+                gyroCurrentRotationRate = e.SensorReading.RotationRate;
+                TimeSpan timeSinceLastUpdate = e.SensorReading.Timestamp - gyroLastUpdateTime;
+                gyroCumulativeRotation += gyroCurrentRotationRate * (float)(timeSinceLastUpdate.TotalSeconds);
+                if (componentOutput)
+                {
+                    gyroCumulativeRotationOutput.X = MathHelper.ToDegrees(gyroCumulativeRotation.X);
+                    gyroCumulativeRotationOutput.Y = MathHelper.ToDegrees(gyroCumulativeRotation.Y);
+                    gyroCumulativeRotationOutput.Z = MathHelper.ToDegrees(gyroCumulativeRotation.Z);
+                }
+                gyroLastUpdateTime = e.SensorReading.Timestamp;
+            }
+        }
+
+        static private void handler_Motion(object sender, SensorReadingEventArgs<MotionReading> e)
+        {
+            motionAttitudeReading = e.SensorReading.Attitude;
+            motionGravity = e.SensorReading.Gravity;
+        }
+    }
+
+    public static class CovertionUtils
+    {
+        public static UnityEngine.Vector3 ToUnityVector3(this Vector3 value)
+        {
+            return new UnityEngine.Vector3(value.X, value.Y, value.Z);
+        }
+
+        public static UnityEngine.Quaternion ToUnityQuaternion(this Quaternion value)
+        {
+            return new UnityEngine.Quaternion(value.X, value.Y, value.Z, value.W);
+        }
+
+    }
+}
---
+++ SensorsFakeDLL/Device.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+
+namespace Device
+{
+    public class Sensors
+    {
+        public static string GetDeviceName
+        {
+            get
+            {
+                return "No Windows Phone";
+            }
+        }
+
+        public static void SetAlwaysIdleMode()
+        {
+            
+        }
+
+        static public float BatteryPercent
+        {
+            get { return 100; }
+        }
+
+        public static bool CompassIsAvailable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public static float CompassMagneticHeading
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public static float CompassTrueHeading
+        {
+            get
+            {
+                return 0f;
+            }
+        }
+
+        public static bool CompassStart()
+        {
+            return false;
+        }
+
+        public static void CompassStop()
+        {
+            
+        }
+
+        public static bool GyroscopeIsAvailable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public static bool GyroscopeStart(bool componentsOutput)
+        {
+            bool result = GyroscopeIsAvailable;
+            return result;
+        }
+
+        static public void GyroscopeStop()
+        {
+
+        }
+
+        static public Vector3 GyroscopeCurrentRotation
+        {
+            get { return Vector3.zero; }
+        }
+
+        static public float GyroscopeCurrentRotationX
+        {
+            get { return 0; }
+        }
+
+        static public float GyroscopeCurrentRotationY
+        {
+            get { return 0; }
+        }
+
+        static public float GyroscopeCurrentRotationZ
+        {
+            get { return 0; }
+        }
+
+        static public bool MotionIsAvailable
+        {
+            get
+            {
+                return false;
+            }
+        }
+        static public bool MotionStart()
+        {
+            bool result = false;
+            return result;
+        }
+
+        static public void MotionStop()
+        {
+            
+        }
+
+        static public Vector3 MotionGravity
+        {
+            get { return Vector3.down * 9.8f; }
+        }
+
+        static public Quaternion MotionAttitude
+        {
+            get { return Quaternion.identity; }
+        }
+
+    }
+}
